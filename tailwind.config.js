360ace014a42fa3fe2b299996b7aec4045b0626c
Victor Almeida <jpvvictor@hotmail.com>
2025-04-29 13:19:22 -0300

    chore: adding tailwind initial config

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#4F46E5',
+          dark: '#4338CA',
+          darker: '#3730A3',
+        },
+        secondary: {
+          DEFAULT: '#FBBF24',
+          dark: '#F59E0B',
+          darker: '#D97706',
+        },
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
