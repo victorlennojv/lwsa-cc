55c1b38b45677302d656c2ff59a6e390524c0373
Victor Almeida <jpvvictor@hotmail.com>
2025-04-29 17:59:40 -0300

    chore: adding vue google api configs and changing tailwind congis

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import vue3GoogleLogin from 'vue3-google-login'
+
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(vue3GoogleLogin, {
+  clientId: '565196005649-10j8cl7o055a3k1kfqd73cmgdiqm4ppv.apps.googleusercontent.com'
+})
 
 app.mount('#app')
