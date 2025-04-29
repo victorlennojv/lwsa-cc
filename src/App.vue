8ec62f8ed133d50b4510b1eb7ecb85f0e6c451eb
Victor Almeida <jpvvictor@hotmail.com>
2025-04-29 17:57:40 -0300

    feat: improving background and screen behaviour

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 </script>
 
 <template>
-  <RouterView />
+  <div class="min-h-full">
+    <div class="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
+      <router-view />
+    </div>
+  </div>
 </template>
