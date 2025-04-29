3166b455a0a600118f3d9ed3648600a851f752f8
Victor Almeida <jpvvictor@hotmail.com>
2025-04-29 13:16:27 -0300

    chore: first commit clean up default vite created files

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import LoginPage from '@/views/LoginPage.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: LoginPage,
+    },
+  ],
+})
+
+export default router
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>Teste</div>
+</template>
+
+<style lang="scss" scoped>
+  
+</style>
