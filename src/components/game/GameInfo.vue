9bad20d4de7fa8aeb51ef563a2d8213f6891a66a
Victor Almeida <jpvvictor@hotmail.com>
2025-04-30 02:53:18 -0300

    feat(game): create GameInfo component to display score and timer
    - Displays total attempts and elapsed time during the game.
    - Highlights results when the game is finished.
    - Adds a reset button to allow restarting the game session.

---
+++ src/components/game/GameInfo.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import BaseButton from '@/components/ui/BaseButton.vue'
+interface Props {
+  turns: number
+  minutes: string | number
+  seconds: string | number
+  isFinished: boolean
+}
+
+defineProps<Props>()
+
+const emit = defineEmits<{
+  (e: 'reset'): void
+}>()
+</script>
+
+<template>
+  <div class="flex justify-center gap-5 mb-5 items-center">
+    <div class="turns">
+      <span class="mr-2">Tentativas: </span>
+      <span :class="['px-2.5 py-1.5 rounded', isFinished ? 'bg-green-600 text-white' : 'bg-gray-300 text-gray-800']">{{ turns }}</span>
+    </div>
+    <div class="time">
+      <span class="mr-2">Tempo total: </span>
+      <span :class="['px-2.5 py-1.5 rounded', isFinished ? 'bg-green-600 text-white' : 'bg-gray-300 text-gray-800']">{{ minutes }}:{{ seconds }}</span>
+    </div>
+    <BaseButton
+      variant="secondary"
+      @click="emit('reset')"
+    >
+      Reiniciar
+    </BaseButton>
+  </div>
+</template>
